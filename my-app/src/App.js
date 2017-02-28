6655227015278815e163d1ec8a07fcddb80bb2f9
Gambadeur <blardonemathieu@yahoo.fr>
2017-02-28 12:15:08 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+// PLACER ICI VOTRE LIEN D'IMPORT
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      
+      //PLACER LES COMPONENTS <> ICI POUR VOTRE PREVIEW PERSO
+      // ATTENTION !!!! SUPPRIMER BIEN VOS COMPONENTS AVANT DE PUSHER !
+      // BISOUS D'AMOUR
       
       </div>
     );
