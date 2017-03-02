1f2182a1d030204d9c4d7dd1a5b40cbcc6f78881
Gambadeur <blardonemathieu@yahoo.fr>
2017-03-02 17:51:17 +0100

    Livraison Navbar responsive

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './top_header/navbar.js'
 // PLACER ICI VOTRE LIEN D'IMPORT
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
-      //PLACER LES COMPONENTS <> ICI POUR VOTRE PREVIEW PERSO
-      // ATTENTION !!!! SUPPRIMER BIEN VOS COMPONENTS AVANT DE PUSHER !
-      // BISOUS D'AMOUR
-      
+      {/*PLACER LES COMPONENTS <> ICI POUR VOTRE PREVIEW PERSO
+      ATTENTION !!!! SUPPRIMER BIEN VOS COMPONENTS AVANT DE PUSHER !
+      BISOUS D'AMOUR
+      */}
       </div>
     );
   }
