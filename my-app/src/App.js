19193f381048c34fb1b8b80e06e9c6fee9f3286d
Sami <ssami.brahim@gmail.com>
2017-03-02 17:40:49 +0100

    ajoutsecondesection

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
-// PLACER ICI VOTRE LIEN D'IMPORT
+import SecondeSection from './SecondeSection/SecondeSection.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
-      //PLACER LES COMPONENTS ICI POUR VOTRE PREVIEW PERSO
-      // ATTENTION !!!! SUPPRIMER BIEN VOS COMPONENTS AVANT DE PUSHER !
-      // BISOUS D'AMOUR
-      
+
+ <SecondeSection> </SecondeSection>
+
       </div>
     );
   }
