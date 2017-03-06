32aca2279683a2eaa2412818a144eb663817ef11
jeffpash <jeffypash@gmail.com>
2017-03-06 12:06:45 +0100

    coooom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,12 +17,10 @@ class App extends Component {
     return (
       <div className="App">
 
-
       {/*PLACER LES COMPONENTS <> ICI POUR VOTRE PREVIEW PERSO
       ATTENTION !!!! SUPPRIMER BIEN VOS COMPONENTS AVANT DE PUSHER !
       BISOUS D'AMOUR
       */}
-
 
 
       </div>
