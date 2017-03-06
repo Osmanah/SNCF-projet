f67cbc43ff5eb09b9b2df7e7fd92b66a350f425b
roula-w <roula.alkayal@hotmail.com>
2017-03-06 12:19:36 +0100

    footer2 new

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
-// PLACER ICI VOTRE LIEN D'IMPORT
+import Footer2 from './Footer2/Footer2.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       
-      //PLACER LES COMPONENTS ICI POUR VOTRE PREVIEW PERSO
+
+      {/* //PLACER LES COMPONENTS ICI POUR VOTRE PREVIEW PERSO
       // ATTENTION !!!! SUPPRIMER BIEN VOS COMPONENTS AVANT DE PUSHER !
-      // BISOUS D'AMOUR
-      
+      // BISOUS D'AMOUR */}
+
       </div>
     );
   }
